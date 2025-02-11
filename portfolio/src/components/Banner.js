a1e6c6832bbe9961b10af2e4be839ff7a46d34f7
Malidu Pahasara <malidupahasara04@gmail.com>
2025-02-11 10:09:20 +0530

    changes

--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -40,7 +40,7 @@ export const Banner = () => {
     } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setIndex(1);
+      setIndex(1); 
       setDelta(500);
     } else {
       setIndex(prevIndex => prevIndex + 1);
